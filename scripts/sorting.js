460dba0becf5a274c8a3dbcda979b40115af4a0a
AndyWavey <andythegodfather@gmail.com>
2022-05-20 11:46:06 +0200

    update

--- scripts/sorting.js
+++ scripts/sorting.js
@@ -37,7 +37,7 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("cases__sorting-container");
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("knap");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
