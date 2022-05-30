8a773fc8c4ca4c5e1845569e96c1ef4eed075e90
AndyWavey <andythegodfather@gmail.com>
2022-05-30 21:36:02 +0200

    update

--- scripts/sorting.js
+++ scripts/sorting.js
@@ -5,7 +5,7 @@ function filterSelection(c) {
   let x, i;
   x = document.getElementsByClassName("cases__grid-item");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
@@ -46,4 +46,4 @@ for (let i = 0; i < btns.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
-}
+} 
